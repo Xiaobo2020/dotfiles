9be1d76f62277e36034d1db1f882e0f7f0756a5a
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-07-30 00:23:25 +0800

    chore: update deps

--- nvim/lua/plugins/hlchunk.lua
+++ nvim/lua/plugins/hlchunk.lua
@@ -2,6 +2,7 @@ return {
   -- indent guides for Neovim hlchunk
   {
     "shellRaining/hlchunk.nvim",
+    enabled = true,
     event = { "BufReadPost", "BufNewFile" },
     opts = {
       chunk = {
