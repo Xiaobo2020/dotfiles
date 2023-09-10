2cad09c75245c65ec74780668f1cf453873f079f
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-09-10 20:50:16 +0800

    feat: update dashboard

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -31,7 +31,7 @@ return {
       dashboard.section.header.opts.hl = "AlphaHeader"
       dashboard.section.buttons.opts.hl = "AlphaButtons"
       dashboard.section.footer.opts.hl = "AlphaFooter"
-      dashboard.opts.layout[1].val = 8
+      dashboard.opts.layout[1].val = 3
       return dashboard
     end,
   },
